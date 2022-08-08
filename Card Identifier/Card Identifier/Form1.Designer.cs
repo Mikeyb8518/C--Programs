e2eff0d637838051f7f5f8ea59b9f66c8425c3de
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2022-08-07 19:28:26 -0700

    Add files via upload

---
+++ Card Identifier/Card Identifier/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Card_Identifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.queenOfDiamonds_Pic = new System.Windows.Forms.PictureBox();
+            this.twoOfHearts_Pic = new System.Windows.Forms.PictureBox();
+            this.sevenOfSpades_Pic = new System.Windows.Forms.PictureBox();
+            this.aceOfClubs_Pic = new System.Windows.Forms.PictureBox();
+            this.fourOfDiamonds_Pic = new System.Windows.Forms.PictureBox();
+            this.answer_Box = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.queenOfDiamonds_Pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoOfHearts_Pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenOfSpades_Pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceOfClubs_Pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourOfDiamonds_Pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // queenOfDiamonds_Pic
+            // 
+            this.queenOfDiamonds_Pic.Image = ((System.Drawing.Image)(resources.GetObject("queenOfDiamonds_Pic.Image")));
+            this.queenOfDiamonds_Pic.Location = new System.Drawing.Point(45, 119);
+            this.queenOfDiamonds_Pic.Name = "queenOfDiamonds_Pic";
+            this.queenOfDiamonds_Pic.Size = new System.Drawing.Size(100, 159);
+            this.queenOfDiamonds_Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.queenOfDiamonds_Pic.TabIndex = 0;
+            this.queenOfDiamonds_Pic.TabStop = false;
+            this.queenOfDiamonds_Pic.Click += new System.EventHandler(this.queenOfDiamonds_Pic_Click);
+            // 
+            // twoOfHearts_Pic
+            // 
+            this.twoOfHearts_Pic.Image = ((System.Drawing.Image)(resources.GetObject("twoOfHearts_Pic.Image")));
+            this.twoOfHearts_Pic.Location = new System.Drawing.Point(204, 119);
+            this.twoOfHearts_Pic.Name = "twoOfHearts_Pic";
+            this.twoOfHearts_Pic.Size = new System.Drawing.Size(100, 159);
+            this.twoOfHearts_Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.twoOfHearts_Pic.TabIndex = 1;
+            this.twoOfHearts_Pic.TabStop = false;
+            this.twoOfHearts_Pic.Click += new System.EventHandler(this.twoOfHearts_Pic_Click);
+            // 
+            // sevenOfSpades_Pic
+            // 
+            this.sevenOfSpades_Pic.Image = ((System.Drawing.Image)(resources.GetObject("sevenOfSpades_Pic.Image")));
+            this.sevenOfSpades_Pic.Location = new System.Drawing.Point(362, 119);
+            this.sevenOfSpades_Pic.Name = "sevenOfSpades_Pic";
+            this.sevenOfSpades_Pic.Size = new System.Drawing.Size(100, 159);
+            this.sevenOfSpades_Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sevenOfSpades_Pic.TabIndex = 2;
+            this.sevenOfSpades_Pic.TabStop = false;
+            this.sevenOfSpades_Pic.Click += new System.EventHandler(this.sevenOfSpades_Pic_Click);
+            // 
+            // aceOfClubs_Pic
+            // 
+            this.aceOfClubs_Pic.Image = ((System.Drawing.Image)(resources.GetObject("aceOfClubs_Pic.Image")));
+            this.aceOfClubs_Pic.Location = new System.Drawing.Point(510, 119);
+            this.aceOfClubs_Pic.Name = "aceOfClubs_Pic";
+            this.aceOfClubs_Pic.Size = new System.Drawing.Size(100, 159);
+            this.aceOfClubs_Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.aceOfClubs_Pic.TabIndex = 3;
+            this.aceOfClubs_Pic.TabStop = false;
+            this.aceOfClubs_Pic.Click += new System.EventHandler(this.aceOfClubs_Pic_Click);
+            // 
+            // fourOfDiamonds_Pic
+            // 
+            this.fourOfDiamonds_Pic.Image = ((System.Drawing.Image)(resources.GetObject("fourOfDiamonds_Pic.Image")));
+            this.fourOfDiamonds_Pic.Location = new System.Drawing.Point(655, 119);
+            this.fourOfDiamonds_Pic.Name = "fourOfDiamonds_Pic";
+            this.fourOfDiamonds_Pic.Size = new System.Drawing.Size(100, 159);
+            this.fourOfDiamonds_Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fourOfDiamonds_Pic.TabIndex = 4;
+            this.fourOfDiamonds_Pic.TabStop = false;
+            this.fourOfDiamonds_Pic.Click += new System.EventHandler(this.fourOfDiamonds_Pic_Click);
+            // 
+            // answer_Box
+            // 
+            this.answer_Box.BackColor = System.Drawing.SystemColors.Menu;
+            this.answer_Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answer_Box.Location = new System.Drawing.Point(45, 344);
+            this.answer_Box.Multiline = true;
+            this.answer_Box.Name = "answer_Box";
+            this.answer_Box.Size = new System.Drawing.Size(710, 37);
+            this.answer_Box.TabIndex = 5;
+            this.answer_Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.answer_Box);
+            this.Controls.Add(this.fourOfDiamonds_Pic);
+            this.Controls.Add(this.aceOfClubs_Pic);
+            this.Controls.Add(this.sevenOfSpades_Pic);
+            this.Controls.Add(this.twoOfHearts_Pic);
+            this.Controls.Add(this.queenOfDiamonds_Pic);
+            this.Name = "Form1";
+            this.Text = "Card Identifier";
+            ((System.ComponentModel.ISupportInitialize)(this.queenOfDiamonds_Pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoOfHearts_Pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenOfSpades_Pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceOfClubs_Pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourOfDiamonds_Pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox queenOfDiamonds_Pic;
+        private System.Windows.Forms.PictureBox twoOfHearts_Pic;
+        private System.Windows.Forms.PictureBox sevenOfSpades_Pic;
+        private System.Windows.Forms.PictureBox aceOfClubs_Pic;
+        private System.Windows.Forms.PictureBox fourOfDiamonds_Pic;
+        private System.Windows.Forms.TextBox answer_Box;
+    }
+}
+
---
+++ Card Identifier/Card Identifier/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Identifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void queenOfDiamonds_Pic_Click(object sender, EventArgs e)
+        {
+            answer_Box.Text = "Queen of Diamonds";
+        }
+
+        private void twoOfHearts_Pic_Click(object sender, EventArgs e)
+        {
+            answer_Box.Text = "Two of Hearts";
+        }
+
+        private void sevenOfSpades_Pic_Click(object sender, EventArgs e)
+        {
+            answer_Box.Text = "Seven of Spades";
+        }
+
+        private void aceOfClubs_Pic_Click(object sender, EventArgs e)
+        {
+            answer_Box.Text = "Ace of Clubs";
+        }
+
+        private void fourOfDiamonds_Pic_Click(object sender, EventArgs e)
+        {
+            answer_Box.Text = "Four of Diamonds";
+        }
+    }
+}
