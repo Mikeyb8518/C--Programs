4e1198aeed03d888d0558c64718d8241366f8e90
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2022-08-07 19:31:58 -0700

    Add files via upload

---
+++ Clickable Number Images/Clickable Number Images/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Clickable_Number_Images
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.onePictureBox_Click = new System.Windows.Forms.PictureBox();
+            this.twoPictureBox_Click = new System.Windows.Forms.PictureBox();
+            this.threePictureBox_Click = new System.Windows.Forms.PictureBox();
+            this.fourPictureBox_Click = new System.Windows.Forms.PictureBox();
+            this.fivePictureBox_Click = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.onePictureBox_Click)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoPictureBox_Click)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threePictureBox_Click)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourPictureBox_Click)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fivePictureBox_Click)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // onePictureBox_Click
+            // 
+            this.onePictureBox_Click.Image = ((System.Drawing.Image)(resources.GetObject("onePictureBox_Click.Image")));
+            this.onePictureBox_Click.Location = new System.Drawing.Point(95, 137);
+            this.onePictureBox_Click.Name = "onePictureBox_Click";
+            this.onePictureBox_Click.Size = new System.Drawing.Size(80, 144);
+            this.onePictureBox_Click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.onePictureBox_Click.TabIndex = 0;
+            this.onePictureBox_Click.TabStop = false;
+            this.onePictureBox_Click.Click += new System.EventHandler(this.onePictureBox_Click_Click);
+            // 
+            // twoPictureBox_Click
+            // 
+            this.twoPictureBox_Click.Image = ((System.Drawing.Image)(resources.GetObject("twoPictureBox_Click.Image")));
+            this.twoPictureBox_Click.Location = new System.Drawing.Point(236, 137);
+            this.twoPictureBox_Click.Name = "twoPictureBox_Click";
+            this.twoPictureBox_Click.Size = new System.Drawing.Size(80, 144);
+            this.twoPictureBox_Click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.twoPictureBox_Click.TabIndex = 1;
+            this.twoPictureBox_Click.TabStop = false;
+            this.twoPictureBox_Click.Click += new System.EventHandler(this.twoPictureBox_Click_Click);
+            // 
+            // threePictureBox_Click
+            // 
+            this.threePictureBox_Click.Image = global::Clickable_Number_Images.Properties.Resources.Three;
+            this.threePictureBox_Click.Location = new System.Drawing.Point(377, 137);
+            this.threePictureBox_Click.Name = "threePictureBox_Click";
+            this.threePictureBox_Click.Size = new System.Drawing.Size(80, 144);
+            this.threePictureBox_Click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.threePictureBox_Click.TabIndex = 2;
+            this.threePictureBox_Click.TabStop = false;
+            this.threePictureBox_Click.Click += new System.EventHandler(this.threePictureBox_Click_Click);
+            // 
+            // fourPictureBox_Click
+            // 
+            this.fourPictureBox_Click.Image = ((System.Drawing.Image)(resources.GetObject("fourPictureBox_Click.Image")));
+            this.fourPictureBox_Click.Location = new System.Drawing.Point(524, 137);
+            this.fourPictureBox_Click.Name = "fourPictureBox_Click";
+            this.fourPictureBox_Click.Size = new System.Drawing.Size(80, 144);
+            this.fourPictureBox_Click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.fourPictureBox_Click.TabIndex = 3;
+            this.fourPictureBox_Click.TabStop = false;
+            this.fourPictureBox_Click.Click += new System.EventHandler(this.fourPictureBox_Click_Click);
+            // 
+            // fivePictureBox_Click
+            // 
+            this.fivePictureBox_Click.Image = ((System.Drawing.Image)(resources.GetObject("fivePictureBox_Click.Image")));
+            this.fivePictureBox_Click.Location = new System.Drawing.Point(668, 137);
+            this.fivePictureBox_Click.Name = "fivePictureBox_Click";
+            this.fivePictureBox_Click.Size = new System.Drawing.Size(80, 144);
+            this.fivePictureBox_Click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.fivePictureBox_Click.TabIndex = 4;
+            this.fivePictureBox_Click.TabStop = false;
+            this.fivePictureBox_Click.Click += new System.EventHandler(this.fivePictureBox_Click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 433);
+            this.Controls.Add(this.fivePictureBox_Click);
+            this.Controls.Add(this.fourPictureBox_Click);
+            this.Controls.Add(this.threePictureBox_Click);
+            this.Controls.Add(this.twoPictureBox_Click);
+            this.Controls.Add(this.onePictureBox_Click);
+            this.Name = "Form1";
+            this.Text = "Clickable Number Images";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.onePictureBox_Click)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoPictureBox_Click)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threePictureBox_Click)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourPictureBox_Click)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fivePictureBox_Click)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox onePictureBox_Click;
+        private System.Windows.Forms.PictureBox twoPictureBox_Click;
+        private System.Windows.Forms.PictureBox threePictureBox_Click;
+        private System.Windows.Forms.PictureBox fourPictureBox_Click;
+        private System.Windows.Forms.PictureBox fivePictureBox_Click;
+    }
+}
+
---
+++ Clickable Number Images/Clickable Number Images/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clickable_Number_Images
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onePictureBox_Click_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("One");
+        }
+
+        private void twoPictureBox_Click_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Two");
+        }
+
+        private void threePictureBox_Click_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Three");
+        }
+
+        private void fourPictureBox_Click_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Four");
+        }
+
+        private void fivePictureBox_Click_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Five");
+        }
+    }
+}
