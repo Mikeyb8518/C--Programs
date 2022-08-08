5686d2a51ae1bec2ed0ca8d741f17c6cb1fdc3fe
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2022-08-07 20:07:40 -0700

    Add files via upload

---
+++ Random Number File Writer/Random Number File Writer/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Random_Number_File_Writer
+{
+    partial class randomNumber_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entry_Label = new System.Windows.Forms.Label();
+            this.user_Entry_Textbox = new System.Windows.Forms.TextBox();
+            this.save_Button = new System.Windows.Forms.Button();
+            this.exit_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // entry_Label
+            // 
+            this.entry_Label.AutoSize = true;
+            this.entry_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.entry_Label.Location = new System.Drawing.Point(12, 33);
+            this.entry_Label.Name = "entry_Label";
+            this.entry_Label.Size = new System.Drawing.Size(187, 18);
+            this.entry_Label.TabIndex = 0;
+            this.entry_Label.Text = "Please Enter a Number:";
+            // 
+            // user_Entry_Textbox
+            // 
+            this.user_Entry_Textbox.BackColor = System.Drawing.SystemColors.Menu;
+            this.user_Entry_Textbox.Location = new System.Drawing.Point(217, 33);
+            this.user_Entry_Textbox.Name = "user_Entry_Textbox";
+            this.user_Entry_Textbox.Size = new System.Drawing.Size(78, 22);
+            this.user_Entry_Textbox.TabIndex = 1;
+            this.user_Entry_Textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // save_Button
+            // 
+            this.save_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save_Button.Location = new System.Drawing.Point(77, 104);
+            this.save_Button.Name = "save_Button";
+            this.save_Button.Size = new System.Drawing.Size(94, 33);
+            this.save_Button.TabIndex = 2;
+            this.save_Button.Text = "Save";
+            this.save_Button.UseVisualStyleBackColor = true;
+            this.save_Button.Click += new System.EventHandler(this.generate_Button_Click);
+            // 
+            // exit_Button
+            // 
+            this.exit_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit_Button.Location = new System.Drawing.Point(201, 104);
+            this.exit_Button.Name = "exit_Button";
+            this.exit_Button.Size = new System.Drawing.Size(94, 33);
+            this.exit_Button.TabIndex = 3;
+            this.exit_Button.Text = "Exit";
+            this.exit_Button.UseVisualStyleBackColor = true;
+            this.exit_Button.Click += new System.EventHandler(this.exit_Button_Click);
+            // 
+            // randomNumber_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 177);
+            this.Controls.Add(this.exit_Button);
+            this.Controls.Add(this.save_Button);
+            this.Controls.Add(this.user_Entry_Textbox);
+            this.Controls.Add(this.entry_Label);
+            this.Name = "randomNumber_Form";
+            this.Text = "Random Number File Writer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label entry_Label;
+        private System.Windows.Forms.TextBox user_Entry_Textbox;
+        private System.Windows.Forms.Button save_Button;
+        private System.Windows.Forms.Button exit_Button;
+    }
+}
+
---
+++ Random Number File Writer/Random Number File Writer/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Random_Number_File_Writer
+{
+    public partial class randomNumber_Form : Form
+    {
+        public randomNumber_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void generate_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Creates a random object
+                Random random = new Random();
+                // Variable 
+                int random_number = 0;
+                // Variable to hold user entry
+                int number = Convert.ToInt32(user_Entry_Textbox.Text);
+                // Declares a stream writer object adn creates file
+                StreamWriter outputfile = File.CreateText("Random Number.txt");
+                // While number is greater than 0
+                while (number > 0)
+                {
+                    // Assigns random number to choose a number from 1-100
+                    random_number = random.Next(1, 101);
+                    // Writes ranndom number to file
+                    outputfile.WriteLine(random_number.ToString());
+                    // Number is decreased by 1
+                    number--;
+                }
+                // Closes file
+                outputfile.Close();
+                // Displays file saved
+                MessageBox.Show("File Saved");
+            }
+            catch (Exception ex)
+            {
+                // Default error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exit_Button_Click(object sender, EventArgs e)
+        {
+            // Closes form
+            this.Close();
+        }
+    }
+}
