22a12590876d26ad813dce9319c318d2e4756b1a
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2022-08-07 20:14:28 -0700

    Add files via upload

---
+++ Sales Analysis/Sales Analysis/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Sales_Analysis
+{
+    partial class sales_Analysis_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exit_Button = new System.Windows.Forms.Button();
+            this.sales_Button = new System.Windows.Forms.Button();
+            this.output_Total_Label = new System.Windows.Forms.Label();
+            this.total_Label = new System.Windows.Forms.Label();
+            this.sales_Groupbox = new System.Windows.Forms.GroupBox();
+            this.sales_Listbox = new System.Windows.Forms.ListBox();
+            this.average_Label = new System.Windows.Forms.Label();
+            this.output_Average_Label = new System.Windows.Forms.Label();
+            this.highest_Sale_Label = new System.Windows.Forms.Label();
+            this.output_High_Label = new System.Windows.Forms.Label();
+            this.lowest_Sale_Label = new System.Windows.Forms.Label();
+            this.output_Lowest_Label = new System.Windows.Forms.Label();
+            this.sales_Groupbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // exit_Button
+            // 
+            this.exit_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit_Button.Location = new System.Drawing.Point(342, 275);
+            this.exit_Button.Name = "exit_Button";
+            this.exit_Button.Size = new System.Drawing.Size(92, 37);
+            this.exit_Button.TabIndex = 10;
+            this.exit_Button.Text = "Exit";
+            this.exit_Button.UseVisualStyleBackColor = true;
+            this.exit_Button.Click += new System.EventHandler(this.exit_Button_Click);
+            // 
+            // sales_Button
+            // 
+            this.sales_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sales_Button.Location = new System.Drawing.Point(186, 275);
+            this.sales_Button.Name = "sales_Button";
+            this.sales_Button.Size = new System.Drawing.Size(106, 37);
+            this.sales_Button.TabIndex = 9;
+            this.sales_Button.Text = "Get Sales";
+            this.sales_Button.UseVisualStyleBackColor = true;
+            this.sales_Button.Click += new System.EventHandler(this.sales_Button_Click);
+            // 
+            // output_Total_Label
+            // 
+            this.output_Total_Label.AutoSize = true;
+            this.output_Total_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output_Total_Label.Location = new System.Drawing.Point(515, 48);
+            this.output_Total_Label.Name = "output_Total_Label";
+            this.output_Total_Label.Size = new System.Drawing.Size(17, 18);
+            this.output_Total_Label.TabIndex = 8;
+            this.output_Total_Label.Text = "0";
+            this.output_Total_Label.Visible = false;
+            // 
+            // total_Label
+            // 
+            this.total_Label.AutoSize = true;
+            this.total_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total_Label.Location = new System.Drawing.Point(429, 48);
+            this.total_Label.Name = "total_Label";
+            this.total_Label.Size = new System.Drawing.Size(51, 18);
+            this.total_Label.TabIndex = 7;
+            this.total_Label.Text = "Total:";
+            // 
+            // sales_Groupbox
+            // 
+            this.sales_Groupbox.Controls.Add(this.sales_Listbox);
+            this.sales_Groupbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sales_Groupbox.Location = new System.Drawing.Point(12, 22);
+            this.sales_Groupbox.Name = "sales_Groupbox";
+            this.sales_Groupbox.Size = new System.Drawing.Size(368, 224);
+            this.sales_Groupbox.TabIndex = 6;
+            this.sales_Groupbox.TabStop = false;
+            this.sales_Groupbox.Text = "Sales";
+            // 
+            // sales_Listbox
+            // 
+            this.sales_Listbox.FormattingEnabled = true;
+            this.sales_Listbox.ItemHeight = 16;
+            this.sales_Listbox.Location = new System.Drawing.Point(14, 26);
+            this.sales_Listbox.Name = "sales_Listbox";
+            this.sales_Listbox.Size = new System.Drawing.Size(338, 180);
+            this.sales_Listbox.TabIndex = 0;
+            // 
+            // average_Label
+            // 
+            this.average_Label.AutoSize = true;
+            this.average_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.average_Label.Location = new System.Drawing.Point(396, 99);
+            this.average_Label.Name = "average_Label";
+            this.average_Label.Size = new System.Drawing.Size(120, 18);
+            this.average_Label.TabIndex = 11;
+            this.average_Label.Text = "Sales Average:";
+            // 
+            // output_Average_Label
+            // 
+            this.output_Average_Label.AutoSize = true;
+            this.output_Average_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output_Average_Label.Location = new System.Drawing.Point(532, 99);
+            this.output_Average_Label.Name = "output_Average_Label";
+            this.output_Average_Label.Size = new System.Drawing.Size(17, 18);
+            this.output_Average_Label.TabIndex = 12;
+            this.output_Average_Label.Text = "0";
+            this.output_Average_Label.Visible = false;
+            // 
+            // highest_Sale_Label
+            // 
+            this.highest_Sale_Label.AutoSize = true;
+            this.highest_Sale_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highest_Sale_Label.Location = new System.Drawing.Point(408, 148);
+            this.highest_Sale_Label.Name = "highest_Sale_Label";
+            this.highest_Sale_Label.Size = new System.Drawing.Size(108, 18);
+            this.highest_Sale_Label.TabIndex = 13;
+            this.highest_Sale_Label.Text = "Highest Sale:";
+            // 
+            // output_High_Label
+            // 
+            this.output_High_Label.AutoSize = true;
+            this.output_High_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output_High_Label.Location = new System.Drawing.Point(532, 148);
+            this.output_High_Label.Name = "output_High_Label";
+            this.output_High_Label.Size = new System.Drawing.Size(17, 18);
+            this.output_High_Label.TabIndex = 14;
+            this.output_High_Label.Text = "0";
+            this.output_High_Label.Visible = false;
+            // 
+            // lowest_Sale_Label
+            // 
+            this.lowest_Sale_Label.AutoSize = true;
+            this.lowest_Sale_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lowest_Sale_Label.Location = new System.Drawing.Point(408, 198);
+            this.lowest_Sale_Label.Name = "lowest_Sale_Label";
+            this.lowest_Sale_Label.Size = new System.Drawing.Size(105, 18);
+            this.lowest_Sale_Label.TabIndex = 15;
+            this.lowest_Sale_Label.Text = "Lowest Sale:";
+            // 
+            // output_Lowest_Label
+            // 
+            this.output_Lowest_Label.AutoSize = true;
+            this.output_Lowest_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output_Lowest_Label.Location = new System.Drawing.Point(532, 198);
+            this.output_Lowest_Label.Name = "output_Lowest_Label";
+            this.output_Lowest_Label.Size = new System.Drawing.Size(17, 18);
+            this.output_Lowest_Label.TabIndex = 16;
+            this.output_Lowest_Label.Text = "0";
+            this.output_Lowest_Label.Visible = false;
+            // 
+            // sales_Analysis_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 331);
+            this.Controls.Add(this.output_Lowest_Label);
+            this.Controls.Add(this.lowest_Sale_Label);
+            this.Controls.Add(this.output_High_Label);
+            this.Controls.Add(this.highest_Sale_Label);
+            this.Controls.Add(this.output_Average_Label);
+            this.Controls.Add(this.average_Label);
+            this.Controls.Add(this.exit_Button);
+            this.Controls.Add(this.sales_Button);
+            this.Controls.Add(this.output_Total_Label);
+            this.Controls.Add(this.total_Label);
+            this.Controls.Add(this.sales_Groupbox);
+            this.Name = "sales_Analysis_Form";
+            this.Text = "Sales Analysis";
+            this.sales_Groupbox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exit_Button;
+        private System.Windows.Forms.Button sales_Button;
+        private System.Windows.Forms.Label output_Total_Label;
+        private System.Windows.Forms.Label total_Label;
+        private System.Windows.Forms.GroupBox sales_Groupbox;
+        private System.Windows.Forms.ListBox sales_Listbox;
+        private System.Windows.Forms.Label average_Label;
+        private System.Windows.Forms.Label output_Average_Label;
+        private System.Windows.Forms.Label highest_Sale_Label;
+        private System.Windows.Forms.Label output_High_Label;
+        private System.Windows.Forms.Label lowest_Sale_Label;
+        private System.Windows.Forms.Label output_Lowest_Label;
+    }
+}
+
---
+++ Sales Analysis/Sales Analysis/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sales_Analysis
+{
+    public partial class sales_Analysis_Form : Form
+    {
+        public sales_Analysis_Form()
+        {
+            InitializeComponent();
+        }
+        // AverageSales method accepts a double array argument
+        // Returns the average value in that array
+        private double AverageSales(double[] average_Sale)
+        {
+            double total = 0;
+            double average;
+
+            for (int index = 0; index < average_Sale.Length; index++)
+            {
+                total = average_Sale[index];
+            }
+
+            average = total / average_Sale.Length;
+            output_Average_Label.Visible = true;
+            output_Average_Label.Text = average.ToString("c");
+
+            return average;
+        }
+        // Highest method accepts a double array argument
+        // Returns the highest value in that array
+        private double Highest(double[] highest_Sale)
+        {
+            double highest = highest_Sale[0];
+
+            for (int index = 1; index < highest_Sale.Length; index++)
+            {
+                if (highest_Sale[index] > highest)
+                {
+                    highest = highest_Sale[index];
+                    output_High_Label.Visible = true;
+                    output_High_Label.Text = highest.ToString("c");
+                }
+            }
+
+            return highest;
+        }
+        // Lowest method accepts a double array argument 
+        // Reutrns the lowest value in that array
+        private double Lowest(double[] lowest_Sale)
+        {
+            double lowest = lowest_Sale[0];
+
+            for (int index = 1; index < lowest_Sale.Length; index++)
+            {
+                if (lowest_Sale[index] < lowest)
+                {
+                    lowest = lowest_Sale[index];
+                    output_Lowest_Label.Visible = true;
+                    output_Lowest_Label.Text = lowest.ToString("c");
+                }
+            }
+
+            return lowest;
+        }
+        // TotalSales method accepts a double array argument
+        // returns the total value in that array
+        private double TotalSales(double[] total_Sales)
+        {
+            double total = 0;
+
+            for (int index = 0; index < total_Sales.Length;index++)
+            {
+                total += total_Sales[index];
+                output_Total_Label.Visible = true;
+                output_Total_Label.Text = total.ToString("c");
+            }
+
+            return total;
+        }
+        
+
+        private void sales_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // number of sales
+                const int SIZE = 7;
+                // Array of sales
+                double[] sales = new double[SIZE];
+                // loop counter
+                int index = 0;
+                // variables to hold average, total, highest/lowest sales
+                double highestSale;
+                double lowestSale;
+                double averageSale;
+                double totalSale;
+                // Declares inputfile variable, open file and gets a streamreader object
+                StreamReader inputFile = File.OpenText("Sales.txt");
+                // Read sales into the array
+                while(!inputFile.EndOfStream && index < sales.Length)
+                {
+                    sales[index] = double.Parse(inputFile.ReadLine());
+                    index++;
+                }
+                // Closes file
+                inputFile.Close();
+                // Displays array in listbox
+                foreach (double value in sales)
+                {
+                    sales_Listbox.Items.Add(value);
+                }
+                // Calls methods to display total, highest, lowest, average
+                totalSale = TotalSales(sales);
+                averageSale = AverageSales(sales);
+                highestSale = Highest(sales);
+                lowestSale = Lowest(sales);
+
+
+            }
+            catch (Exception ex)
+            {
+                // Displays error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exit_Button_Click(object sender, EventArgs e)
+        {
+            // Closes form
+            this.Close();
+        }
+    }
+}
