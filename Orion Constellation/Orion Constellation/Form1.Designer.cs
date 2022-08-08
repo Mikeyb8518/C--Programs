3738754072c6f3dc489223a8435fb85aeb6bc70a
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2022-08-07 19:55:14 -0700

    Add files via upload

---
+++ Orion Constellation/Orion Constellation/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Orion_Constellation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.orion_Pic = new System.Windows.Forms.PictureBox();
+            this.show_Button = new System.Windows.Forms.Button();
+            this.hide_Button = new System.Windows.Forms.Button();
+            this.exit_Button = new System.Windows.Forms.Button();
+            this.betelgeuse_Label = new System.Windows.Forms.Label();
+            this.meissa_Label = new System.Windows.Forms.Label();
+            this.alnitak_Label = new System.Windows.Forms.Label();
+            this.alnilam_Label = new System.Windows.Forms.Label();
+            this.mintaka_Label = new System.Windows.Forms.Label();
+            this.sapiah_Label = new System.Windows.Forms.Label();
+            this.rigel_Label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.orion_Pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // orion_Pic
+            // 
+            this.orion_Pic.Image = ((System.Drawing.Image)(resources.GetObject("orion_Pic.Image")));
+            this.orion_Pic.Location = new System.Drawing.Point(0, -2);
+            this.orion_Pic.Name = "orion_Pic";
+            this.orion_Pic.Size = new System.Drawing.Size(682, 660);
+            this.orion_Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orion_Pic.TabIndex = 0;
+            this.orion_Pic.TabStop = false;
+            // 
+            // show_Button
+            // 
+            this.show_Button.Location = new System.Drawing.Point(64, 703);
+            this.show_Button.Name = "show_Button";
+            this.show_Button.Size = new System.Drawing.Size(114, 50);
+            this.show_Button.TabIndex = 1;
+            this.show_Button.Text = "Show Star Names";
+            this.show_Button.UseVisualStyleBackColor = true;
+            this.show_Button.Click += new System.EventHandler(this.show_Button_Click);
+            // 
+            // hide_Button
+            // 
+            this.hide_Button.Location = new System.Drawing.Point(278, 703);
+            this.hide_Button.Name = "hide_Button";
+            this.hide_Button.Size = new System.Drawing.Size(114, 50);
+            this.hide_Button.TabIndex = 2;
+            this.hide_Button.Text = "Hide Star Names";
+            this.hide_Button.UseVisualStyleBackColor = true;
+            this.hide_Button.Click += new System.EventHandler(this.hide_Button_Click);
+            // 
+            // exit_Button
+            // 
+            this.exit_Button.Location = new System.Drawing.Point(499, 703);
+            this.exit_Button.Name = "exit_Button";
+            this.exit_Button.Size = new System.Drawing.Size(114, 50);
+            this.exit_Button.TabIndex = 3;
+            this.exit_Button.Text = "Exit";
+            this.exit_Button.UseVisualStyleBackColor = true;
+            this.exit_Button.Click += new System.EventHandler(this.exit_Button_Click);
+            // 
+            // betelgeuse_Label
+            // 
+            this.betelgeuse_Label.AutoSize = true;
+            this.betelgeuse_Label.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.betelgeuse_Label.Location = new System.Drawing.Point(77, 41);
+            this.betelgeuse_Label.Name = "betelgeuse_Label";
+            this.betelgeuse_Label.Size = new System.Drawing.Size(79, 17);
+            this.betelgeuse_Label.TabIndex = 4;
+            this.betelgeuse_Label.Text = "Betelgeuse";
+            this.betelgeuse_Label.Visible = false;
+            // 
+            // meissa_Label
+            // 
+            this.meissa_Label.AutoSize = true;
+            this.meissa_Label.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.meissa_Label.Location = new System.Drawing.Point(457, 92);
+            this.meissa_Label.Name = "meissa_Label";
+            this.meissa_Label.Size = new System.Drawing.Size(52, 17);
+            this.meissa_Label.TabIndex = 5;
+            this.meissa_Label.Text = "Meissa";
+            this.meissa_Label.Visible = false;
+            // 
+            // alnitak_Label
+            // 
+            this.alnitak_Label.AutoSize = true;
+            this.alnitak_Label.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.alnitak_Label.Location = new System.Drawing.Point(152, 327);
+            this.alnitak_Label.Name = "alnitak_Label";
+            this.alnitak_Label.Size = new System.Drawing.Size(50, 17);
+            this.alnitak_Label.TabIndex = 6;
+            this.alnitak_Label.Text = "Alnitak";
+            this.alnitak_Label.Visible = false;
+            // 
+            // alnilam_Label
+            // 
+            this.alnilam_Label.AutoSize = true;
+            this.alnilam_Label.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.alnilam_Label.Location = new System.Drawing.Point(275, 279);
+            this.alnilam_Label.Name = "alnilam_Label";
+            this.alnilam_Label.Size = new System.Drawing.Size(53, 17);
+            this.alnilam_Label.TabIndex = 7;
+            this.alnilam_Label.Text = "Alnilam";
+            this.alnilam_Label.Visible = false;
+            // 
+            // mintaka_Label
+            // 
+            this.mintaka_Label.AutoSize = true;
+            this.mintaka_Label.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.mintaka_Label.Location = new System.Drawing.Point(400, 279);
+            this.mintaka_Label.Name = "mintaka_Label";
+            this.mintaka_Label.Size = new System.Drawing.Size(57, 17);
+            this.mintaka_Label.TabIndex = 8;
+            this.mintaka_Label.Text = "Mintaka";
+            this.mintaka_Label.Visible = false;
+            // 
+            // sapiah_Label
+            // 
+            this.sapiah_Label.AutoSize = true;
+            this.sapiah_Label.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.sapiah_Label.Location = new System.Drawing.Point(126, 550);
+            this.sapiah_Label.Name = "sapiah_Label";
+            this.sapiah_Label.Size = new System.Drawing.Size(44, 17);
+            this.sapiah_Label.TabIndex = 9;
+            this.sapiah_Label.Text = "Saiph";
+            this.sapiah_Label.Visible = false;
+            // 
+            // rigel_Label
+            // 
+            this.rigel_Label.AutoSize = true;
+            this.rigel_Label.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.rigel_Label.Location = new System.Drawing.Point(457, 505);
+            this.rigel_Label.Name = "rigel_Label";
+            this.rigel_Label.Size = new System.Drawing.Size(40, 17);
+            this.rigel_Label.TabIndex = 10;
+            this.rigel_Label.Text = "Rigel";
+            this.rigel_Label.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 791);
+            this.Controls.Add(this.rigel_Label);
+            this.Controls.Add(this.sapiah_Label);
+            this.Controls.Add(this.mintaka_Label);
+            this.Controls.Add(this.alnilam_Label);
+            this.Controls.Add(this.alnitak_Label);
+            this.Controls.Add(this.meissa_Label);
+            this.Controls.Add(this.betelgeuse_Label);
+            this.Controls.Add(this.exit_Button);
+            this.Controls.Add(this.hide_Button);
+            this.Controls.Add(this.show_Button);
+            this.Controls.Add(this.orion_Pic);
+            this.Name = "Form1";
+            this.Text = "Orion Constellation";
+            ((System.ComponentModel.ISupportInitialize)(this.orion_Pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox orion_Pic;
+        private System.Windows.Forms.Button show_Button;
+        private System.Windows.Forms.Button hide_Button;
+        private System.Windows.Forms.Button exit_Button;
+        private System.Windows.Forms.Label betelgeuse_Label;
+        private System.Windows.Forms.Label meissa_Label;
+        private System.Windows.Forms.Label alnitak_Label;
+        private System.Windows.Forms.Label alnilam_Label;
+        private System.Windows.Forms.Label mintaka_Label;
+        private System.Windows.Forms.Label sapiah_Label;
+        private System.Windows.Forms.Label rigel_Label;
+    }
+}
+
---
+++ Orion Constellation/Orion Constellation/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Orion_Constellation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void show_Button_Click(object sender, EventArgs e)
+        {
+            betelgeuse_Label.Visible = true;
+            meissa_Label.Visible = true;
+            alnitak_Label.Visible = true;
+            alnilam_Label.Visible = true;
+            mintaka_Label.Visible = true;
+            sapiah_Label.Visible = true;
+            rigel_Label.Visible = true;
+        }
+
+        private void hide_Button_Click(object sender, EventArgs e)
+        {
+            betelgeuse_Label.Visible = false;
+            meissa_Label.Visible = false;
+            alnitak_Label.Visible = false;
+            alnilam_Label.Visible = false;
+            mintaka_Label.Visible = false;
+            sapiah_Label.Visible = false;
+            rigel_Label.Visible = false;
+        }
+
+        private void exit_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
