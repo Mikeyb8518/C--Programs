283dc1ff3b5a34f6eaaa1bc296f87ac7fbffe2de
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2022-08-07 20:30:22 -0700

    Add files via upload

---
+++ World Series Champions/World Series Champions/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace World_Series_Champions
+{
+    partial class champions_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.teams_Groupbox = new System.Windows.Forms.GroupBox();
+            this.teams_Listbox = new System.Windows.Forms.ListBox();
+            this.teams_Groupbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // teams_Groupbox
+            // 
+            this.teams_Groupbox.Controls.Add(this.teams_Listbox);
+            this.teams_Groupbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teams_Groupbox.Location = new System.Drawing.Point(16, 22);
+            this.teams_Groupbox.Name = "teams_Groupbox";
+            this.teams_Groupbox.Size = new System.Drawing.Size(304, 416);
+            this.teams_Groupbox.TabIndex = 0;
+            this.teams_Groupbox.TabStop = false;
+            this.teams_Groupbox.Text = "MLB Teams";
+            // 
+            // teams_Listbox
+            // 
+            this.teams_Listbox.FormattingEnabled = true;
+            this.teams_Listbox.ItemHeight = 18;
+            this.teams_Listbox.Location = new System.Drawing.Point(20, 35);
+            this.teams_Listbox.Name = "teams_Listbox";
+            this.teams_Listbox.Size = new System.Drawing.Size(264, 364);
+            this.teams_Listbox.TabIndex = 0;
+            this.teams_Listbox.SelectedIndexChanged += new System.EventHandler(this.teams_Listbox_SelectedIndexChanged);
+            // 
+            // champions_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 450);
+            this.Controls.Add(this.teams_Groupbox);
+            this.Name = "champions_Form";
+            this.Text = "World Series Champions";
+            this.Load += new System.EventHandler(this.champions_Form_Load);
+            this.teams_Groupbox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox teams_Groupbox;
+        private System.Windows.Forms.ListBox teams_Listbox;
+    }
+}
+
---
+++ World Series Champions/World Series Champions/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace World_Series_Champions
+{
+    public partial class champions_Form : Form
+    {
+        // List of strings to hold champlist
+        List<string> champList = new List<string>();
+        // variable to hold loop counter
+        int i = 0;
+
+
+        public champions_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void champions_Form_Load(object sender, EventArgs e)
+        {
+            // Declared string variable
+            string display;
+            // Declares StreamReader variable to open text files
+            StreamReader teamFile = File.OpenText("Teams.txt");
+            StreamReader champFile = File.OpenText("WorldSeriesWinners.txt");
+            // Display is assigned to teamFile and reads contents as long as not empty
+            while ((display = teamFile.ReadLine()) != null)
+            {
+                // Displays teamFile to teams_Listbox
+                teams_Listbox.Items.Add(display);
+            }
+            // Declares string varibale and assigns/ reads champFile
+            string champs = champFile.ReadLine();
+            // While champsFile is not empty
+            while (champs != null)
+            {
+                // Adds to the champList 
+                champList.Add(champs);
+                champs = champFile.ReadLine();
+                // Increments I for every readline in champsFile
+                i++;
+            }
+
+        }
+
+        private void teams_Listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Assigns display variable to teams_Listbox selected item 
+            string display = teams_Listbox.SelectedItems[0].ToString();
+            // Variables to hold counters
+            int index = 0;
+            int count = 0;
+            // While index is less than I
+            while (index < i)
+            {
+                // If champList index is equal to selected item
+                if (champList[index] == display)
+                {
+                    // Count increases
+                    count++;
+                }
+                // Increments index placement 
+                index++;
+            }
+            // Dislays selected team and count of world series won
+            MessageBox.Show(display + " Won " + count + " World Series");
+        }
+
+    }
+}
