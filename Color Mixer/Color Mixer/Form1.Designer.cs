81cd97ba7b1fd48c701bd248904880014c5fc573
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2022-08-07 19:34:42 -0700

    Add files via upload

---
+++ Color Mixer/Color Mixer/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Color_Mixer
+{
+    partial class color_Mixer_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.first_Color_GroupBox = new System.Windows.Forms.GroupBox();
+            this.second_Color_GroupBox = new System.Windows.Forms.GroupBox();
+            this.red_RadioButton1 = new System.Windows.Forms.RadioButton();
+            this.blue_RadioButton1 = new System.Windows.Forms.RadioButton();
+            this.yellow_RadioButton1 = new System.Windows.Forms.RadioButton();
+            this.red_RadioButton2 = new System.Windows.Forms.RadioButton();
+            this.blue_RadioButton2 = new System.Windows.Forms.RadioButton();
+            this.yellow_RadioButton2 = new System.Windows.Forms.RadioButton();
+            this.mix_Button = new System.Windows.Forms.Button();
+            this.exit_Button = new System.Windows.Forms.Button();
+            this.first_Color_GroupBox.SuspendLayout();
+            this.second_Color_GroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // first_Color_GroupBox
+            // 
+            this.first_Color_GroupBox.Controls.Add(this.yellow_RadioButton1);
+            this.first_Color_GroupBox.Controls.Add(this.blue_RadioButton1);
+            this.first_Color_GroupBox.Controls.Add(this.red_RadioButton1);
+            this.first_Color_GroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.first_Color_GroupBox.Location = new System.Drawing.Point(50, 126);
+            this.first_Color_GroupBox.Name = "first_Color_GroupBox";
+            this.first_Color_GroupBox.Size = new System.Drawing.Size(276, 200);
+            this.first_Color_GroupBox.TabIndex = 0;
+            this.first_Color_GroupBox.TabStop = false;
+            this.first_Color_GroupBox.Text = "Select the First Color";
+            // 
+            // second_Color_GroupBox
+            // 
+            this.second_Color_GroupBox.Controls.Add(this.yellow_RadioButton2);
+            this.second_Color_GroupBox.Controls.Add(this.blue_RadioButton2);
+            this.second_Color_GroupBox.Controls.Add(this.red_RadioButton2);
+            this.second_Color_GroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.second_Color_GroupBox.Location = new System.Drawing.Point(385, 126);
+            this.second_Color_GroupBox.Name = "second_Color_GroupBox";
+            this.second_Color_GroupBox.Size = new System.Drawing.Size(276, 200);
+            this.second_Color_GroupBox.TabIndex = 1;
+            this.second_Color_GroupBox.TabStop = false;
+            this.second_Color_GroupBox.Text = "Select the Second Color";
+            // 
+            // red_RadioButton1
+            // 
+            this.red_RadioButton1.AutoSize = true;
+            this.red_RadioButton1.Location = new System.Drawing.Point(57, 42);
+            this.red_RadioButton1.Name = "red_RadioButton1";
+            this.red_RadioButton1.Size = new System.Drawing.Size(56, 22);
+            this.red_RadioButton1.TabIndex = 0;
+            this.red_RadioButton1.TabStop = true;
+            this.red_RadioButton1.Text = "Red";
+            this.red_RadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // blue_RadioButton1
+            // 
+            this.blue_RadioButton1.AutoSize = true;
+            this.blue_RadioButton1.Location = new System.Drawing.Point(57, 90);
+            this.blue_RadioButton1.Name = "blue_RadioButton1";
+            this.blue_RadioButton1.Size = new System.Drawing.Size(58, 22);
+            this.blue_RadioButton1.TabIndex = 1;
+            this.blue_RadioButton1.TabStop = true;
+            this.blue_RadioButton1.Text = "Blue";
+            this.blue_RadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // yellow_RadioButton1
+            // 
+            this.yellow_RadioButton1.AutoSize = true;
+            this.yellow_RadioButton1.Location = new System.Drawing.Point(57, 139);
+            this.yellow_RadioButton1.Name = "yellow_RadioButton1";
+            this.yellow_RadioButton1.Size = new System.Drawing.Size(72, 22);
+            this.yellow_RadioButton1.TabIndex = 2;
+            this.yellow_RadioButton1.TabStop = true;
+            this.yellow_RadioButton1.Text = "Yellow";
+            this.yellow_RadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // red_RadioButton2
+            // 
+            this.red_RadioButton2.AutoSize = true;
+            this.red_RadioButton2.Location = new System.Drawing.Point(64, 42);
+            this.red_RadioButton2.Name = "red_RadioButton2";
+            this.red_RadioButton2.Size = new System.Drawing.Size(56, 22);
+            this.red_RadioButton2.TabIndex = 1;
+            this.red_RadioButton2.TabStop = true;
+            this.red_RadioButton2.Text = "Red";
+            this.red_RadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // blue_RadioButton2
+            // 
+            this.blue_RadioButton2.AutoSize = true;
+            this.blue_RadioButton2.Location = new System.Drawing.Point(64, 90);
+            this.blue_RadioButton2.Name = "blue_RadioButton2";
+            this.blue_RadioButton2.Size = new System.Drawing.Size(58, 22);
+            this.blue_RadioButton2.TabIndex = 2;
+            this.blue_RadioButton2.TabStop = true;
+            this.blue_RadioButton2.Text = "Blue";
+            this.blue_RadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // yellow_RadioButton2
+            // 
+            this.yellow_RadioButton2.AutoSize = true;
+            this.yellow_RadioButton2.Location = new System.Drawing.Point(64, 139);
+            this.yellow_RadioButton2.Name = "yellow_RadioButton2";
+            this.yellow_RadioButton2.Size = new System.Drawing.Size(72, 22);
+            this.yellow_RadioButton2.TabIndex = 3;
+            this.yellow_RadioButton2.TabStop = true;
+            this.yellow_RadioButton2.Text = "Yellow";
+            this.yellow_RadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // mix_Button
+            // 
+            this.mix_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mix_Button.Location = new System.Drawing.Point(247, 345);
+            this.mix_Button.Name = "mix_Button";
+            this.mix_Button.Size = new System.Drawing.Size(96, 29);
+            this.mix_Button.TabIndex = 2;
+            this.mix_Button.Text = "Mix";
+            this.mix_Button.UseVisualStyleBackColor = true;
+            this.mix_Button.Click += new System.EventHandler(this.mix_Button_Click);
+            // 
+            // exit_Button
+            // 
+            this.exit_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit_Button.Location = new System.Drawing.Point(359, 345);
+            this.exit_Button.Name = "exit_Button";
+            this.exit_Button.Size = new System.Drawing.Size(96, 29);
+            this.exit_Button.TabIndex = 3;
+            this.exit_Button.Text = "Exit";
+            this.exit_Button.UseVisualStyleBackColor = true;
+            this.exit_Button.Click += new System.EventHandler(this.exit_Button_Click);
+            // 
+            // color_Mixer_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 450);
+            this.Controls.Add(this.exit_Button);
+            this.Controls.Add(this.mix_Button);
+            this.Controls.Add(this.second_Color_GroupBox);
+            this.Controls.Add(this.first_Color_GroupBox);
+            this.Name = "color_Mixer_Form";
+            this.Text = "Color Mixer";
+            this.first_Color_GroupBox.ResumeLayout(false);
+            this.first_Color_GroupBox.PerformLayout();
+            this.second_Color_GroupBox.ResumeLayout(false);
+            this.second_Color_GroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox first_Color_GroupBox;
+        private System.Windows.Forms.RadioButton yellow_RadioButton1;
+        private System.Windows.Forms.RadioButton blue_RadioButton1;
+        private System.Windows.Forms.RadioButton red_RadioButton1;
+        private System.Windows.Forms.GroupBox second_Color_GroupBox;
+        private System.Windows.Forms.RadioButton yellow_RadioButton2;
+        private System.Windows.Forms.RadioButton blue_RadioButton2;
+        private System.Windows.Forms.RadioButton red_RadioButton2;
+        private System.Windows.Forms.Button mix_Button;
+        private System.Windows.Forms.Button exit_Button;
+    }
+}
+
---
+++ Color Mixer/Color Mixer/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_Mixer
+{
+    public partial class color_Mixer_Form : Form
+    {
+        public color_Mixer_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void mix_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //If user selects Red and Blue colors to mix
+                if (red_RadioButton1.Checked && blue_RadioButton2.Checked)
+                {
+                    // Form color will display Purple
+                    this.BackColor = Color.Purple;
+                }
+                // If user selects Red and Yellow colors to mix
+                else if (red_RadioButton1.Checked && yellow_RadioButton2.Checked)
+                {
+                    // Form color will display Orange 
+                    this.BackColor = Color.Orange;
+                }
+                // If user selects Blue and Yellow colors to mix
+                else if (blue_RadioButton1.Checked && yellow_RadioButton2.Checked)
+                {
+                    // Form color will display Green
+                    this.BackColor = Color.Green;
+                }
+                // If user selects both Red
+                else if (red_RadioButton1.Checked && red_RadioButton2.Checked)
+                {
+                    // Form color will display Red
+                    this.BackColor = Color.Red;
+                }
+                // If user selects both blue
+                else if (blue_RadioButton1.Checked && blue_RadioButton2.Checked)
+                {
+                    // Form color will display Blue
+                    this.BackColor = Color.Blue;
+                }
+                // If user selects both Yellow
+                else if (yellow_RadioButton1.Checked && yellow_RadioButton2.Checked)
+                {
+                    // Form color will display Yellow
+                    this.BackColor = Color.Yellow;
+                }
+                else
+                {
+                    // Message to display 2 mixing colors
+                    MessageBox.Show("Please select 2 mixing colors");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Display default error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exit_Button_Click(object sender, EventArgs e)
+        {
+            // Closes the form
+            this.Close();
+        }
+    }
+}
